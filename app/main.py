b5fbbd3614ccc956434aea75cdf8a2a575b9c59e
Kavi Sekhon <kavi.skhon@gmail.com>
2022-12-25 12:10:07 -0800

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import coloredlogs
+import logging
+
+coloredlogs.install()
+
+LOG_FILENAME = "app.log"
+logging.basicConfig(
+    filename=LOG_FILENAME,
+    force=True,
+    level=logging.INFO,
+    format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
+)
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    log.info("Done")
+    pass
+
+
+if __name__ == "__main__":
+    main()
